a8ef3b0d3babc411ac29fb846f431fb98e030008
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2023-09-21 23:11:18 +0300

    task review

--- src/main/java/com/trendyol/bootcamp/spring/ch04/config/RewardsConfig.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch04/config/RewardsConfig.java
@@ -10,6 +10,7 @@ import com.trendyol.bootcamp.spring.ch04.service.RewardNetwork;
 import com.trendyol.bootcamp.spring.ch04.service.RewardNetworkImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -20,43 +21,14 @@ import javax.sql.DataSource;
  * - Set the base package to pick up all the classes we have annotated so far.
  * - Save all changes, Re-run the RewardNetworkTests.  It should now pass.
  */
+@ComponentScan({"com"})
 @Configuration
 public class RewardsConfig {
 
-	DataSource dataSource;
 
-	@Autowired
-	public RewardsConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+
 		
-	@Bean
-	public RewardNetwork rewardNetwork(){
-		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
-	}
-	
-	@Bean
-	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository();
-		repository.setDataSource(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository();
-		repository.setDataSource(dataSource);
-		return repository;
-	}
+
 	
 	// TODO-02: Remove all of the @Bean methods above.
 	// - Remove the code that autowires DataSource as well.
--- src/main/java/com/trendyol/bootcamp/spring/ch04/service/RewardNetworkImpl.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch04/service/RewardNetworkImpl.java
@@ -9,6 +9,8 @@ import com.trendyol.bootcamp.spring.ch04.domain.RewardConfirmation;
 import com.trendyol.bootcamp.spring.ch04.repository.account.AccountRepository;
 import com.trendyol.bootcamp.spring.ch04.repository.restaurant.RestaurantRepository;
 import com.trendyol.bootcamp.spring.ch04.repository.reward.RewardRepository;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 /**
  * Rewards an Account for Dining at a Restaurant.
@@ -28,7 +30,7 @@ import com.trendyol.bootcamp.spring.ch04.repository.reward.RewardRepository;
  * - Inject all 3 dependencies.  Decide if you should use field
  *   injection or constructor injection.
  */
-
+@Component
 public class RewardNetworkImpl implements RewardNetwork {
 
 	private AccountRepository accountRepository;
